c5293355942f4b1a9a91292a74cc72e8abe43d9c
Aafaque Nazir <aafaquenazir@gmail.com>
2025-04-28 18:38:46 +0530

    fixes.

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,14 @@ import About from './pages/About';
 import Skills from './pages/Skills';
 import Project from './pages/Project';  
 import Contact from './pages/Contact';
-import TYP from './pages/TYP';
-import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/thank-you" element={<TYP/>} />
-        <Route path="/" element={<About/>} />
-      </Routes>
+      <About/>
       <Skills />  
       <Project />
       <Contact />
-
     </>
   );
 }
